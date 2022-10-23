1d714ac12a6a4909ca693688d998ae8b85aaf73b
JadwigaAdamiec <79207166+JadwigaAdamiec@users.noreply.github.com>
2022-10-23 15:04:57 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,8 @@
-def chess_board(n, k):
+
+def chess_board(n,k):
+    for j in range(n):
+        for i in range(k):
+            print((" " * k + "#" * k) * n)
+        for i in range(k):
+            print(("#" * k + " " * k) * n)
     pass
