55721496e9caed76f0c1ae75688d1a2658c606c2
JadwigaAdamiec <79207166+JadwigaAdamiec@users.noreply.github.com>
2022-10-23 15:32:52 +0200

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,9 @@
 def envelope(n):
+    print("*" * (2*n +1))
+    for i in range(n-1):
+        print ("*" + " " * i + "*" + " " * (2*n-3-2*i) + "*" + " " * i + "*")
+    print("*" + " " * (n-1) + "*" + " " * (n-1) + "*")
+    for j in range(n-1):
+        print ("*" + " " * (n-j-2) + "*" + " " * (n-3+2*j) + "*" + " " * (n-j-2) + "*")
+    print("*" * (2*n +1))
     pass
