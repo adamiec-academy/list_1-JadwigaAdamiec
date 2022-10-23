a8e52b329b4936d02dc92059106aef81beb1a75d
Wojciech Adamiec <56234206+WojciechAdamiec@users.noreply.github.com>
2022-10-23 23:48:06 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,8 +1,6 @@
-
 def chess_board(n,k):
     for j in range(n):
         for i in range(k):
             print((" " * k + "#" * k) * n)
         for i in range(k):
             print(("#" * k + " " * k) * n)
-    pass
