4398770f584570172d9dc3e4fbd7f2baa49eb15c
JadwigaAdamiec <79207166+JadwigaAdamiec@users.noreply.github.com>
2022-10-25 21:10:31 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,7 +1,7 @@
 def cross(n):
     for i in range(n):
-        print(" " * (n-1), "*" * n)
+        print(" " * (n-1) + "*" * n)
     for i in range(n):
         print("*" * n * 3)
     for i in range(n):
-        print(" " * (n-1), "*" * n)
+        print(" " * (n-1) + "*" * n)
